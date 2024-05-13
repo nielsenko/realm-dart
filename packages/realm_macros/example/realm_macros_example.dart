9f8f83ddc22d1945606d65ccfcade608092ec7c5
Kasper Overgård Nielsen <kasper@byolimit.com>
2024-05-13 18:52:22 +0200

    Move macro up from lib/src to lib/ (seems to be a bug)

--- packages/realm_macros/example/realm_macros_example.dart
+++ packages/realm_macros/example/realm_macros_example.dart
@@ -1,4 +1,12 @@
+//import 'package:macros/macros.dart';
 import 'package:realm_macros/realm_macros.dart';
 
+@RealmModel2()
+class TestModel {
+  final int id;
+}
+
 void main() {
+  var model = TestModel(id: 1);
+  print(model);
 }
