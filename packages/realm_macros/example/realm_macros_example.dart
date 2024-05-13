59390c8b5e26961c154b6f8bf43287830673795b
Kasper Overgård Nielsen <kasper@byolimit.com>
2024-05-13 17:38:17 +0200

    Enable macros feature
    - Bump min SDK to ^3.5.0-0
    - Update analysis_options to enable experimental macros feature
    - Add basic realm_macros package
    - Add new RealmModel2 macro (just a copy of the DataClass example)

---
+++ packages/realm_macros/example/realm_macros_example.dart
@@ -0,0 +1,4 @@
+import 'package:realm_macros/realm_macros.dart';
+
+void main() {
+}
---
+++ packages/realm_macros/test/macros_test.dart
@@ -0,0 +1,14 @@
+import 'package:realm_macros/realm_macros.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+    });
+  });
+}
