1bcab8d79be60ea9cc98d4b51e3f42bed5eb2c67
Kasper Overgård Nielsen <kasper@byolimit.com>
2024-05-17 15:21:31 +0200

    Print backlink schema property

--- packages/realm_macros/example/just_backlink.dart
+++ packages/realm_macros/example/just_backlink.dart
@@ -8,5 +8,17 @@ class Foo {
 
 void main() {
   var foo = Foo();
+
+  final schema = Foo.dogsProperty.schema;
+  print('name               : ${schema.name}');
+  print('propertyType       : ${schema.propertyType}');
+  print('optional           : ${schema.optional}');
+  print('mapTo              : ${schema.mapTo}');
+  print('primaryKey         : ${schema.primaryKey}');
+  print('indexType          : ${schema.indexType}');
+  print('linkTarget         : ${schema.linkTarget}');
+  print('linkOriginProperty : ${schema.linkOriginProperty}');
+  print('collectionType     : ${schema.collectionType}');
+
   print(foo.dogs);
 }
