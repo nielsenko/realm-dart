a284768aa4f08e78f5c5ff2047ed44f81ced5841
Kasper Overgård Nielsen <kasper@byolimit.com>
2024-05-17 15:05:00 +0200

    Fix extra , bug

--- packages/realm_macros/lib/backlink_macro.dart
+++ packages/realm_macros/lib/backlink_macro.dart
@@ -4,12 +4,12 @@ import 'dart:async';
 
 import 'package:macros/macros.dart';
 import 'package:realm_dart/realm.dart';
+import 'package:realm_macros/realm_model_macro.dart'; // ignore: unused_import
 
 import 'property.dart';
 import 'utils.dart';
 
-macro 
-class BacklinkMacro implements MethodDeclarationsMacro, MethodDefinitionMacro {
+macro class BacklinkMacro implements MethodDeclarationsMacro, MethodDefinitionMacro {
   final String fieldName;
 
   const BacklinkMacro(this.fieldName);
@@ -21,22 +21,20 @@ class BacklinkMacro implements MethodDeclarationsMacro, MethodDefinitionMacro {
       final iterableType = await builder.resolveByType<Iterable>(Uri.parse('dart:core'));
       final returnType = await builder.resolve(method.returnType.code);
       if (await returnType.isSubtypeOf(iterableType)) {
-        final propertyType = await builder.resolveByType<Property>();
-        final schemaPropertyType = await builder.resolveByType<SchemaProperty>();
         builder.declareInType(DeclarationCode.fromParts([
           'static const ',
           method.identifier.name,
           'Property = ',
-          propertyType,
+          (await builder.typeAnnotationOf<Property>()).code,
           '(',
-          schemaPropertyType,
+          (await builder.typeAnnotationOf<SchemaProperty>()).code,
           '(',
           "'${method.identifier.name}', ",
           await builder.resolveIdentifier(
             Uri.parse('package:realm_macros/realm_model_macro.dart'),
             'realmPropertyTypeLinkingObjects',
           ),
-          ", linkOriginProperty: '$fieldName', ",
+          ", linkOriginProperty: '$fieldName'",
           ', collectionType: ',
           await builder.resolveIdentifier(
             Uri.parse('package:realm_macros/realm_model_macro.dart'),
